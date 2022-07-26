495c77d23d79c9c11b1f68d3b716f6e7d466aeee
Albertino Vieira <8140346@estg.ipp.pt>
2022-07-26 18:47:41 +0100

    added moesif

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+const moesif = require('moesif-nodejs');
+
+const moesifMiddleware = moesif({
+    applicationId: 'eyJhcHAiOiI0ODc6MzQ0IiwidmVyIjoiMi4wIiwib3JnIjoiNDIwOjMxNCIsImlhdCI6MTY1NjYzMzYwMH0.6z3DYrwMK2u87zMqVrXKjGnjtkc9q4jWleQZVGQrYjk',
+
+    // Optional hook to link API calls to users
+
+});
+
+// 3. Enable the Moesif middleware to start logging incoming API Calls
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(moesifMiddleware);
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
